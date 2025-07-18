8e6f1dc0fad745db0d472fa6c200e266123dc655
pentasoft <pentasoftrepo@gmail.com>
2025-07-18 11:51:18 +0530

    update logout

--- app/Livewire/Components/User/Partials/Header.php
+++ app/Livewire/Components/User/Partials/Header.php
@@ -2,10 +2,20 @@
 
 namespace App\Livewire\Components\User\Partials;
 
+use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
 
 class Header extends Component
 {
+
+
+    public function logout()
+    {
+        Auth::logout();
+        session()->invalidate();
+        session()->regenerateToken();
+        return $this->redirectRoute('login', navigate: true);
+    }
     public function render()
     {
         return view('livewire.components.user.partials.header');
--- resources/views/livewire/components/user/partials/header.blade.php
+++ resources/views/livewire/components/user/partials/header.blade.php
@@ -443,16 +443,17 @@
                             </li><!-- li -->
                             <li class="dropdown-divider"></li><!-- li -->
                             <li>
-                                <a href="login.html">
+                                <a href="#" wire:click.prevent="logout">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
                                         fill="currentColor" class="bi bi-power" viewBox="0 0 16 16">
                                         <path d="M7.5 1v7h1V1z" />
                                         <path
                                             d="M3 8.812a5 5 0 0 1 2.578-4.375l-.485-.874A6 6 0 1 0 11 3.616l-.501.865A5 5 0 1 1 3 8.812" />
-                                    </svg><!-- power -->
+                                    </svg>
                                     <span>Log Out</span>
                                 </a>
-                            </li><!-- li -->
+                            </li>
+
                         </ul><!-- .tyn-list-links -->
                     </div><!-- .dropdown-menu -->
                 </li><!-- .tyn-appbar-item -->
